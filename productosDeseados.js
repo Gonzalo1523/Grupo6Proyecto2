416521696370d35d891dcac801addae9a141d657
Gonzalo Trejo <tgonzalosebastian18@gmail.com>
2023-05-02 22:35:31 -0300

    Subo pagina favoritos que ya es responsive

--- productosDeseados.js
+++ productosDeseados.js
@@ -6,7 +6,7 @@ botonAgregar.addEventListener('click', () => {
 	const nuevaFila = document.createElement('tr');
 	nuevaFila.setAttribute('id', `fila${contadorId}`);
 	
-	nuevaFila.innerHTML = `<td class="align-middle"><img src="https://via.placeholder.com/150x150" alt="Producto 1"></td>
+	nuevaFila.innerHTML = `<td class="align-middle d-none d-md-table-cell text-center"><img src="https://via.placeholder.com/150x150" alt="Producto 1"></td>
 	<td class="align-middle">
 		<h5 class="mb-0">Producto 1</h5>
 		<p class="mb-0">Descripción del producto 1.</p>
@@ -14,8 +14,6 @@ botonAgregar.addEventListener('click', () => {
 	<td class="align-middle">$10.00</td>
 	<td class="align-middle">
 		<button class="btn btn-primary">Comprar</button>
-	</td>
-	<td class="align-middle">
 		<button class="btn btn-danger">Eliminar Producto</button>
 	</td>`;
 	contenedorCards.appendChild(nuevaFila);
