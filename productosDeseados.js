6554de7c55ba0c9cce47fce7016c5783b9bcb676
Gonzalo Trejo <tgonzalosebastian18@gmail.com>
2023-05-02 22:22:17 -0300

    Aqui agrego nuevo formato de tabla para productosDeseados con sus funciones pero no es responsive

--- productosDeseados.js
+++ productosDeseados.js
@@ -3,33 +3,27 @@ const contenedorCards = document.getElementById('contenedorCards');
 let contadorId = 0;
 
 botonAgregar.addEventListener('click', () => {
-	const nuevaCard = document.createElement('div');
-	nuevaCard.classList.add('col-sm-6', 'col-md-4', 'col-lg-3');
-	nuevaCard.setAttribute('id', `card${contadorId}`);
-
-	const cardContenido = `
-    <div class="card bg-secondary text-white">
-      <img src="..." class="card-img-top" alt="Imagen no Encontrada"
-        style="max-width: 100%; height: auto;">
-      <div class="card-body">
-        <h5 class="card-title">Titulo del Producto</h5>
-        <p class="card-text">Descripcion de el producto</p>
-        <a href="#" class="btn btn-primary d-block">¡Comprar!</a>
-        <br>
-        <a href="#" class="btn btn-primary d-block botonEliminar">Eliminar Producto</a>
-      </div>
-    </div>`;
-
-	nuevaCard.innerHTML = cardContenido;
-	contenedorCards.appendChild(nuevaCard);
+	const nuevaFila = document.createElement('tr');
+	nuevaFila.setAttribute('id', `fila${contadorId}`);
+	
+	nuevaFila.innerHTML = `<td class="align-middle"><img src="https://via.placeholder.com/150x150" alt="Producto 1"></td>
+	<td class="align-middle">
+		<h5 class="mb-0">Producto 1</h5>
+		<p class="mb-0">Descripción del producto 1.</p>
+	</td>
+	<td class="align-middle">$10.00</td>
+	<td class="align-middle">
+		<button class="btn btn-primary">Comprar</button>
+	</td>
+	<td class="align-middle">
+		<button class="btn btn-danger">Eliminar Producto</button>
+	</td>`;
+	contenedorCards.appendChild(nuevaFila);
 	contadorId++;
 
-
-	const botonEliminar = nuevaCard.querySelector('.botonEliminar');
+	const botonEliminar = nuevaFila.querySelector('.btn-danger');
 	botonEliminar.addEventListener('click', () => {
-		const cardAEliminar = nuevaCard;
-		contenedorCards.removeChild(cardAEliminar);
+		const filaAEliminar = nuevaFila;
+		contenedorCards.removeChild(filaAEliminar);
 	});
-
 });
-
