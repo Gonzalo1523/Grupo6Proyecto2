32aee23ee1419340d664487c7d6ef036e327d488
Gonzalo Trejo <tgonzalosebastian18@gmail.com>
2023-05-09 21:53:01 -0300

    agrego commit con cambios en la fila

--- productosDeseados.js
+++ productosDeseados.js
@@ -11,15 +11,17 @@ const ActualizarLista = () => {
 	let productoVista = Productos.map((producto) => {
 		let productoVIsta = `
 		<tr>
-			<td class="align-middle d-none d-md-table-cell text-center"><img src="https://via.placeholder.com/150x150" alt="Producto 1"></td>
+			<td class="align-middle d-none d-md-table-cell text-center"><img src="${producto.url}" alt="Producto 1"></td>
 			<td class="align-middle">
 				<h5 class="mb-0">${producto.nombre} 1</h5>
 				<p class="mb-0">${producto.descripcion}</p>
 			</td>
 			<td class="align-middle">${producto.precio}</td>
 			<td class="align-middle">
-				<button class="btn btn-primary">Comprar</button>
-				<button class="btn btn-danger">Eliminar Producto</button>
+		    <div class= "btn-group-vertical">
+			    <button class="btn btn-primary">Comprar</button>
+				<button class="btn btn-danger">Eliminar </button>
+			</div>
 			</td>
 		</tr>
 		`
