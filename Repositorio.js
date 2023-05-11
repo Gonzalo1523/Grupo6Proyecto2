b01e89ab0b43e828b6041b46cd06318e083003b9
Nicolas-Veliz <nikolas13zzz3@gmail.com>
2023-05-11 19:02:39 -0300

    subeindo modificaciones en index

--- Repositorio.js
+++ Repositorio.js
@@ -1,27 +1,35 @@
 const Producto = {   
     "Productos":[
         {
-            "nombre": "taladro",
+            "nombre": "Taladro",
             "codigo": 0,
             "url": "https://bergalloypastrone.vtexassets.com/arquivos/ids/156060/Taladro-Percutor-Skil-6555-13mm-550w-Velocidad-Variable-1.jpg?v=637710274357000000",
-            "categoria": "herramientas electricas"
+            "descripcion": "Con el taladro eléctrico Philco TP711 podrás realizar múltiples tareas en diversas superficies de un modo práctico y sencillo.",
+            "precio": "$ 10.200",
+            "categoria": "Herramientas Electricas"
         },
         {
             "nombre": "Pistola Neumatica",
             "codigo": 1,
             "url": "https://siglo21myh.com.ar/wp-content/uploads/2022/11/pistola-neumatica-nc4258-1.png",
-            "categoria": "herramientas Neumatica"
+            "descripcion": "Diseño ergonométrico. Ideal para uso semi-profesional. Sistema “Rocking Dog”. Mango Soft Grip antideslizante.",
+            "precio": "$ 17.870",
+            "categoria": "Herramientas Neumatica"
         },
         {
             "nombre": "Portalampara",
             "codigo": 2,
-            "url": "https://bergalloypastrone.vtexassets.com/arquivos/ids/156060/Taladro-Percutor-Skil-6555-13mm-550w-Velocidad-Variable-1.jpg?v=637710274357000000",
-            "categoria": "Electronica del hogar"
+            "url": "https://storage.googleapis.com/fs_static/stores/s730_cz1b/images/products/m/porta-lampara-negro-3-p_98655.jpg",
+            "descripcion": "Portalámpara Mignon E-14 3 Piezas NEGRO. Diametro: 30 mm , Largo : 50 mm. Base con rosca para niple 3/8",
+            "precio": "$ 550",
+            "categoria": "Electronica del Hogar"
         },
         {
             "nombre": "Tijera de Podar",
             "codigo": 3,
             "url": "https://media.istockphoto.com/id/182440238/es/foto/cizallas.jpg?s=612x612&w=0&k=20&c=VmXDTT7OPwJjPcX4NolsvvMrWdjomO-SG6qK3F985HM=",
+            "descripcion": "Medidas: Largo Total 17,8 cm. MEdida de Hoja: 6,2 cm. Medida de MANGO: 11,6 cm. Peso> 110 Gramos. Mango ergonómico curvo, con topes internos. Más comodidad, firmeza y seguridad durante el uso. Compacta y eficiente.",
+            "precio": "$ 2.500",
             "categoria": "Jardineria"
         }
     ],
--- js/productos.js
+++ js/productos.js
@@ -1,26 +1,45 @@
 const seleccionarProducto = (codigo) => {
-  let producto = productos.find((producto)=>producto.codigo == codigo);
-  console.log(producto);
+  let producto = productos.find((producto) => producto.codigo == codigo);
+  Swal.fire({
+    position: "top-center",
+    icon: "success",
+    title: "Su producto se agrego correctamente a Favoritos",
+    showConfirmButton: false,
+    timer: 1500,
+  });
 };
 const produc = document.getElementById("catalogo");
 let productos = JSON.parse(localStorage.getItem("Productos"));
 function mostrarCard() {
   let prod = productos.map((Producto) => {
     let card = `
-    <div class="col-sm-12 col-md-6 col-lg-6 my-2">
-      <div class="card shadow bg-light border border-0">
+    <div class="card-body bg-light border border-5" style="width: 18rem; text-align: center; margin: 30px 30px;">
         <div class="card-body">
-        <a href="eror404.html"><img src="${Producto.url}" class='imgCard'></a>
-          <h5 class="card-title">${Producto.nombre}</h5>
-          <h6 class="card-title">${"Categoria: " + Producto.categoria}</h6>
+        <a href="eror404.html"><img src="${Producto.url}" class='imgCard' class="card-img-top"></a>
+            <h5 class="card-title">${Producto.nombre}</h5>
+            <h6 class="card-title">${Producto.descripcion}</h6>
+            <h6 class="card-title">${Producto.precio}</h6>
+            <h6 class="card-title">${"Categoria: " + Producto.categoria}</h6>
         </div>
         <div class="card-footer">
-            <button class="btn btn-primary " onClick=seleccionarProducto(${Producto.codigo})>Favoritos</button>
+            <button class="btn btn-primary" onClick=mostrarMensaje()>Agregar al Carrito</button>
+            <button class="btn btn-warning" onClick=seleccionarProducto(${Producto.codigo})>Favoritos</button>
+            <button class="btn btn-success" onClick=seleccionarProducto(${Producto.codigo})>Comprar</button>
         </div>
-      </div>
+    </div>
     </div>`;
     return card;
   });
   produc.innerHTML = prod.join("");
 }
 mostrarCard();
+
+function mostrarMensaje(mensaje) {
+  Swal.fire({
+    position: "top-center",
+    icon: "success",
+    title: "Su producto se agrego correctamente al Carrito ",
+    showConfirmButton: false,
+    timer: 1500,
+  });
+} 
