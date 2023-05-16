9cdacd518c35078aee1dc09e09fea9ccc9a7a877
Sanchez Agustin <agustin.sanchez@scania.com>
2023-05-16 18:59:11 -0300

    union con develop

---
+++ Repositorio.js
@@ -0,0 +1,166 @@
+const Producto = {   
+    "Productos":[
+        {
+            "nombre": "Taladro",
+            "codigo": 0,
+            "url": "https://bergalloypastrone.vtexassets.com/arquivos/ids/156060/Taladro-Percutor-Skil-6555-13mm-550w-Velocidad-Variable-1.jpg?v=637710274357000000",
+            "categoria": "herramientas electricas",
+            "descripcion":"El taladro percutor es un dispositivo versátil que ayuda a los usuarios en sus trabajos en la casa y el taller." ,
+            "precio": "$11.999" 
+        },
+        {
+            "nombre": "Pistola Neumatica",
+            "codigo": 1,
+            "url": "https://siglo21myh.com.ar/wp-content/uploads/2022/11/pistola-neumatica-nc4258-1.png",
+            "categoria": "herramientas Neumatica",
+            "descripcion": "La llave de impacto neumática Einhell TC-PW 340 ofrece mucha potencia para trabajos en el hogar, el taller y el garaje. La robusta llave de impacto tiene un par máximo de 340 Nm.",
+            "precio":"$22.800" 
+        },
+        {
+            "nombre": "Portalampara",
+            "codigo": 2,
+            "url": "https://http2.mlstatic.com/D_NQ_NP_603036-MLA42734836165_072020-W.webp",
+            "categoria": "Electronica del hogar",
+            "descripcion": "Portalamparas en desmontable de 3 piezas en Combo" ,
+            "precio": "$500"
+        },
+        {
+            "nombre": "Tijera de Podar",
+            "codigo": 3,
+            "url": "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTw6qe8_Dk7qitqkg2Ts1EN-ntsawg9l4uUOpNeAJWb-FpAkdNI",
+            "categoria": "Jardineria",
+            "descripcion":"Tijera para poda 53 cm ligera mangos tubulares, Truper. Diseño ligero y resistente, Muesca para corte de ramas" ,
+            "precio": "$11.500"
+        },
+        
+        {
+            "nombre": "Grifo de Cocina",
+            "codigo": 4,
+            "url": "https://http2.mlstatic.com/D_NQ_NP_787552-MLA49795423644_042022-O.webp",
+            "categoria": "Mobiliario para Baños",
+            "descripcion": "Los grifos de la marca Peirano tienen como respaldo más de 80 años de trayectoria en el rubro sanitario. Además, cuentan con calidad certificada ISO 9001. Este modelo 910/M es una solución para darle un toque diferente a tu cocina.",
+            "precio": "$14.499"
+        },
+        
+        {
+            "nombre": "Tapa de Asiento Inodoro",
+            "codigo": 5,
+            "url": "https://http2.mlstatic.com/D_NQ_NP_750622-MLA40503861386_012020-O.webp",
+            "categoria": "Mobiliario para Baños",
+            "descripcion": "Roca es referente a nivel mundial en fabricación de productos para el hogar desde 1917, destacando en el mercado por la calidad y durabilidad de sus productos.",
+            "precio": "$22.000"
+        },
+        
+        {
+            "nombre": "Mueble para baño",
+            "codigo": 6,
+            "url": "https://http2.mlstatic.com/D_NQ_NP_941158-MLA49922230202_052022-O.webp",
+            "categoria": "Mobiliario para Baños",
+            "descripcion":"El mueble para baño es práctico y cómodo, ya que suma espacio de guardado e ideal para cualquier tipo de baño." ,
+            "precio": "$28.200"
+        },
+        
+        {
+            "nombre": "Termofusora 800w maletin",
+            "codigo": 7,
+            "url": "https://http2.mlstatic.com/D_NQ_NP_844241-MLA47060866678_082021-O.webp",
+            "categoria": "herramientas electricas",
+            "descripcion": "Cuerpo metálico aluminio, robusto y liviano, mango aislado ergonómico recubierto en PVC, Alimentación:220 V/ 50Hz",
+            "precio": "$8.699"
+        },
+        
+        {
+            "nombre": "Juego Set mecha Sierra",
+            "codigo": 8,
+            "url":"https://http2.mlstatic.com/D_NQ_NP_628832-MLA41613910301_052020-O.webp",
+            "categoria": "herramientas electricas",
+            "descripcion":"Juego de sierras copas de la mas alta calidad para tu trabajo de taller y tambien para el hogar" ,
+            "precio": "$4.999"
+        },
+        
+        {
+            "nombre": "Amoladora Neumatica Recta",
+            "codigo": 9,
+            "url": "https://http2.mlstatic.com/D_NQ_NP_772960-MLA53023455381_122022-O.webp",
+            "categoria": "herramientas Neumatica" ,
+            "descripcion": "Diseño compacto, permite maniobrar en espacios reducidos, el escape trasero evita virutas metalicas peligrosas para el usuario",
+            "precio": "$10.600" 
+        },
+        
+        {
+            "nombre": "Clavaroda Engrampadora Neumatica",
+            "codigo": 10,
+            "url": "https://http2.mlstatic.com/D_NQ_NP_774750-MLA51563008079_092022-O.webp",
+            "categoria": "herramientas Neumatica" ,
+            "descripcion": "Fabricada con materiales de 1ra calidad, la clavadora engrampadora es la mejor herramienta a la hora de realizar tareas de carpinteria",
+            "precio": "$10.700"
+        },
+        
+        {  
+            "nombre": "Caloventor Electrico",
+            "codigo": 11,
+            "url": "https://http2.mlstatic.com/D_NQ_NP_989420-MLA46873069301_072021-O.webp",
+            "categoria": "Electronica del hogar",
+            "descripcion": "Creá un ambiente acogedor con el Caloventor Exahome CV03. Por ser eléctrico no necesitarás combustible ni mantenimiento especial, lo cual lo convierte en un indispensable para pasar los meses fríos del año",
+            "precio": "$6.800"
+        },
+        
+        {
+            "nombre": "Anafe Eléctrico",
+            "codigo": 12,
+            "url": "https://http2.mlstatic.com/D_NQ_NP_742112-MLA48689975896_122021-O.webp",
+            "categoria": "Electronica del hogar",
+            "descripcion":"Marca argentina y reconocida, Winco logra reinventarse al ofrecer productos adaptados a las necesidades y con la mejor relación precio-calidad del mercado. Disfrutá de la simpleza y facilidad que te ofrece la marca en este anafe que no puede faltar en tu cocina." ,
+            "precio": "$11.999"
+        },
+        {
+            "nombre": "Kit de Jardineria Maletin",
+            "codigo": 13,
+            "url": "https://http2.mlstatic.com/D_NQ_NP_734657-MLA52127340268_102022-O.webp",
+            "categoria": "Jardineria",
+            "descripcion":"Con este excelente kit, encontra todo lo que necesitas para reacondicionar tu jardin." ,
+            "precio": "$8.999"
+        },
+        
+        {
+            "nombre": "Desmalezadora Electrica",
+            "codigo": 14,
+            "url": "https://http2.mlstatic.com/D_NQ_NP_670208-MLA46192592379_052021-O.webp",
+            "categoria": "Jardineria",
+            "descripcion": "Seguridad al alcance de tu mano. El protector de seguridad evitará que te dañes y resguarda al equipo, ya que limita el largo del hilo de corte evitando posibles sobrecargas.",
+            "precio": "$32.999"
+        }
+        
+    ],
+    "Usuarios":[
+        {
+            "Nombre": "David Antonio Moreno",
+            "NombreUsuario": "DavidMoreno",
+            "Contraseña": "1234",
+            "rol": "Administrador"
+        },
+        {
+            "Nombre": "Gonzalo Sebastian Trejo",
+            "NombreUsuario": "GonzaloTrejo",
+            "Contraseña": "1234",
+            "rol": "Usuario"
+        },
+        {
+            "Nombre": "Nestor Nicolas Veliz",
+            "NombreUsuario": "NicolasVeliz",
+            "Contraseña": "1234",
+            "rol": "Usuario"
+        },
+        {
+            "Nombre": "Juan Agustin Sanchez",
+            "NombreUsuario": "AgustinSanchez",
+            "Contraseña": "1234",
+            "rol": "Usuario"
+        }
+    ]
+}
+let productosLS = localStorage.getItem("Productos");
+if(productosLS==null){
+    localStorage.setItem("Productos",JSON.stringify(Producto.Productos))
+    localStorage.setItem("Usuarios",JSON.stringify(Producto.Usuarios)) 
+}
