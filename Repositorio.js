0064a5614bdf212efe86fb18fc165a4850e777be
Nicolas-Veliz <nikolas13zzz3@gmail.com>
2023-05-16 20:01:49 -0300

    subiendo cambios en index y agregando error404

--- Repositorio.js
+++ Repositorio.js
@@ -4,7 +4,7 @@ const Producto = {
             "nombre": "Taladro",
             "codigo": 0,
             "url": "https://bergalloypastrone.vtexassets.com/arquivos/ids/156060/Taladro-Percutor-Skil-6555-13mm-550w-Velocidad-Variable-1.jpg?v=637710274357000000",
-            "descripcion": "Con el taladro eléctrico Philco TP711 podrás realizar múltiples tareas en diversas superficies de un modo práctico y sencillo.",
+            "descripcion": "Con el taladro eléctrico Skil TP711 podrás realizar múltiples tareas en diversas superficies de un modo práctico y sencillo.",
             "precio": "$ 10.200",
             "categoria": "Herramientas Electricas"
         },
@@ -28,10 +28,34 @@ const Producto = {
             "nombre": "Tijera de Podar",
             "codigo": 3,
             "url": "https://media.istockphoto.com/id/182440238/es/foto/cizallas.jpg?s=612x612&w=0&k=20&c=VmXDTT7OPwJjPcX4NolsvvMrWdjomO-SG6qK3F985HM=",
-            "descripcion": "Medidas: Largo Total 17,8 cm. MEdida de Hoja: 6,2 cm. Medida de MANGO: 11,6 cm. Peso> 110 Gramos. Mango ergonómico curvo, con topes internos. Más comodidad, firmeza y seguridad durante el uso. Compacta y eficiente.",
+            "descripcion": "Mango ergonómico curvo, con topes internos. Más comodidad, firmeza y seguridad durante el uso.",
             "precio": "$ 2.500",
             "categoria": "Jardineria"
-        }
+        },
+        {
+            "nombre": "Portalampara",
+            "codigo": 2,
+            "url": "https://storage.googleapis.com/fs_static/stores/s730_cz1b/images/products/m/porta-lampara-negro-3-p_98655.jpg",
+            "descripcion": "Portalámpara Mignon E-14 3 Piezas NEGRO. Diametro: 30 mm , Largo : 50 mm. Base con rosca para niple 3/8",
+            "precio": "$ 550",
+            "categoria": "Electronica del Hogar"
+        },
+        {
+            "nombre": "Portalampara",
+            "codigo": 2,
+            "url": "https://storage.googleapis.com/fs_static/stores/s730_cz1b/images/products/m/porta-lampara-negro-3-p_98655.jpg",
+            "descripcion": "Portalámpara Mignon E-14 3 Piezas NEGRO. Diametro: 30 mm , Largo : 50 mm. Base con rosca para niple 3/8",
+            "precio": "$ 550",
+            "categoria": "Electronica del Hogar"
+        },
+        {
+            "nombre": "Portalampara",
+            "codigo": 2,
+            "url": "https://storage.googleapis.com/fs_static/stores/s730_cz1b/images/products/m/porta-lampara-negro-3-p_98655.jpg",
+            "descripcion": "Portalámpara Mignon E-14 3 Piezas NEGRO. Diametro: 30 mm , Largo : 50 mm. Base con rosca para niple 3/8",
+            "precio": "$ 550",
+            "categoria": "Electronica del Hogar"
+        },
     ],
     "Usuarios":[
         {
