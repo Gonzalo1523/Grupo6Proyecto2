b153bfc96b94db4f1bcd6031ba63280248e4a7c7
Nicolas-Veliz <nikolas13zzz3@gmail.com>
2023-05-16 20:51:40 -0300

    solucionando conflictos

--- Repositorio.js
+++ Repositorio.js
@@ -4,79 +4,29 @@ const Producto = {
             "nombre": "Taladro",
             "codigo": 0,
             "url": "https://bergalloypastrone.vtexassets.com/arquivos/ids/156060/Taladro-Percutor-Skil-6555-13mm-550w-Velocidad-Variable-1.jpg?v=637710274357000000",
-<<<<<<< HEAD
-            "descripcion": "Con el taladro eléctrico Skil TP711 podrás realizar múltiples tareas en diversas superficies de un modo práctico y sencillo.",
-            "precio": "$ 10.200",
-            "categoria": "Herramientas Electricas"
-=======
             "categoria": "herramientas electricas",
             "descripcion":"El taladro percutor es un dispositivo versátil que ayuda a los usuarios en sus trabajos en la casa y el taller." ,
             "precio": "$11.999" 
->>>>>>> 687b6ecdaeca21900001823acd32a7b437589772
         },
         {
             "nombre": "Pistola Neumatica",
             "codigo": 1,
             "url": "https://siglo21myh.com.ar/wp-content/uploads/2022/11/pistola-neumatica-nc4258-1.png",
-<<<<<<< HEAD
-            "descripcion": "Diseño ergonométrico. Ideal para uso semi-profesional. Sistema “Rocking Dog”. Mango Soft Grip antideslizante.",
-            "precio": "$ 17.870",
-            "categoria": "Herramientas Neumatica"
-=======
             "categoria": "herramientas Neumatica",
             "descripcion": "La llave de impacto neumática Einhell TC-PW 340 ofrece mucha potencia para trabajos en el hogar, el taller y el garaje. La robusta llave de impacto tiene un par máximo de 340 Nm.",
             "precio":"$22.800" 
->>>>>>> 687b6ecdaeca21900001823acd32a7b437589772
         },
         {
             "nombre": "Portalampara",
             "codigo": 2,
-<<<<<<< HEAD
-            "url": "https://storage.googleapis.com/fs_static/stores/s730_cz1b/images/products/m/porta-lampara-negro-3-p_98655.jpg",
-            "descripcion": "Portalámpara Mignon E-14 3 Piezas NEGRO. Diametro: 30 mm , Largo : 50 mm. Base con rosca para niple 3/8",
-            "precio": "$ 550",
-            "categoria": "Electronica del Hogar"
-=======
             "url": "https://http2.mlstatic.com/D_NQ_NP_603036-MLA42734836165_072020-W.webp",
             "categoria": "Electronica del hogar",
             "descripcion": "Portalamparas en desmontable de 3 piezas en Combo" ,
             "precio": "$500"
->>>>>>> 687b6ecdaeca21900001823acd32a7b437589772
         },
         {
             "nombre": "Tijera de Podar",
             "codigo": 3,
-<<<<<<< HEAD
-            "url": "https://media.istockphoto.com/id/182440238/es/foto/cizallas.jpg?s=612x612&w=0&k=20&c=VmXDTT7OPwJjPcX4NolsvvMrWdjomO-SG6qK3F985HM=",
-            "descripcion": "Mango ergonómico curvo, con topes internos. Más comodidad, firmeza y seguridad durante el uso.",
-            "precio": "$ 2.500",
-            "categoria": "Jardineria"
-        },
-        {
-            "nombre": "Portalampara",
-            "codigo": 2,
-            "url": "https://storage.googleapis.com/fs_static/stores/s730_cz1b/images/products/m/porta-lampara-negro-3-p_98655.jpg",
-            "descripcion": "Portalámpara Mignon E-14 3 Piezas NEGRO. Diametro: 30 mm , Largo : 50 mm. Base con rosca para niple 3/8",
-            "precio": "$ 550",
-            "categoria": "Electronica del Hogar"
-        },
-        {
-            "nombre": "Portalampara",
-            "codigo": 2,
-            "url": "https://storage.googleapis.com/fs_static/stores/s730_cz1b/images/products/m/porta-lampara-negro-3-p_98655.jpg",
-            "descripcion": "Portalámpara Mignon E-14 3 Piezas NEGRO. Diametro: 30 mm , Largo : 50 mm. Base con rosca para niple 3/8",
-            "precio": "$ 550",
-            "categoria": "Electronica del Hogar"
-        },
-        {
-            "nombre": "Portalampara",
-            "codigo": 2,
-            "url": "https://storage.googleapis.com/fs_static/stores/s730_cz1b/images/products/m/porta-lampara-negro-3-p_98655.jpg",
-            "descripcion": "Portalámpara Mignon E-14 3 Piezas NEGRO. Diametro: 30 mm , Largo : 50 mm. Base con rosca para niple 3/8",
-            "precio": "$ 550",
-            "categoria": "Electronica del Hogar"
-        },
-=======
             "url": "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTw6qe8_Dk7qitqkg2Ts1EN-ntsawg9l4uUOpNeAJWb-FpAkdNI",
             "categoria": "Jardineria",
             "descripcion":"Tijera para poda 53 cm ligera mangos tubulares, Truper. Diseño ligero y resistente, Muesca para corte de ramas" ,
@@ -181,7 +131,6 @@ const Producto = {
             "precio": "$32.999"
         }
         
->>>>>>> 687b6ecdaeca21900001823acd32a7b437589772
     ],
     "Usuarios":[
         {
