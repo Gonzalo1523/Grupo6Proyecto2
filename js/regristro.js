1ded273e7bd07b946515d6a41ef3cf07654ba957
AgustinSanchez1 <agustinsanchezdev@gmail.com>
2023-05-17 22:59:19 -0300

    ultima version del login y registrarse

--- js/regristro.js
+++ js/regristro.js
@@ -1,18 +1,55 @@
-const signupForm=document.querySelector(`#signupForm`)
-signupForm.addEventListener(`submit`,(e)=>{
-e.preventDefault()
-const name=document.querySelector(`#name`).value
-const email=document.querySelector(`#email`).value
-const password=document.querySelector(`#password`).value
- 
-const Users=JSON.parse(localStorage.getItem(`users`)) || []
-const isUserRegistered=users.find(user=> user.email===email)
-if(isUserRegistered){
-    return alert(`El usuario ya esta registrado`)
+const signupForm = document.querySelector(`#signupForm`);
+signupForm.addEventListener(`submit`, (e) => {
+  e.preventDefault();
+  const Nombre = document.querySelector(`#Nombre`).value;
+  const NombreUsuario = document.querySelector(`#NombreUsuario`).value;
+  const email = document.querySelector(`#email`).value;
+  const Contraseña = document.querySelector(`#Contraseña`).value;
 
-}
-Users,push({name:name ,email:email ,paassword:password})
-localStorage.setItem(`users`, JSON.stringify(Users))
-alert(`registro exitoso`)
+  const usuario = {
+    Contraseña: Contraseña,
+    Nombre: Nombre,
+    NombreUsuario: NombreUsuario,
+    email: email,
+    rol: "Usuario",
+  };
 
-})
+  const Usuarios = JSON.parse(localStorage.getItem(`Usuarios`));
+
+  if (Usuarios != undefined && Usuarios.length > 0) {
+    const UsuarioRegistrado = Usuarios.find(
+      (usuario) => usuario.email === email
+    );
+
+    if (UsuarioRegistrado) {
+        signupForm.reset();
+        return Swal.fire({
+            icon: "error",
+            title: "Error",
+            text: "Usuario ya registrasdo",
+        });
+    }
+
+    Usuarios.push(usuario);
+    localStorage.setItem(`Usuarios`, JSON.stringify(Usuarios));
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: `Usuario Registrado`,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+    signupForm.reset();
+  } else {
+    let Usuarios = [usuario];
+    localStorage.setItem("Usuarios", JSON.stringify(Usuarios));
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: `Usuario Registrado`,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+    signupForm.reset();
+  }
+});
